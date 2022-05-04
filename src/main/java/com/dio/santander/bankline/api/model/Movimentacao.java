64901ffac963094175b9c8da0bf556bc4202cfa3
silviahbf <silviahfernandes@gmail.com>
2022-05-04 19:47:45 -0300

    versão final do projeto

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -2,6 +2,7 @@ package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -27,6 +28,9 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
 	
 	public Integer getId() {
 		return id;
@@ -58,6 +62,11 @@ public class Movimentacao {
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
-	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 	
 }
